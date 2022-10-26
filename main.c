30cc7da731fbd5597348fede8eafdb139f0b58d9
junya uchizono <junya19991215@icloud.com>
2022-10-26 23:50:36 +0900

    fix

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
+
+#define PATH_SIZE 512
 
 int main() {
+    char path_name[PATH_SIZE];
+    getcwd(path_name, PATH_SIZE);
+    printf("current dir : %s\n", path_name);
     printf("Hello, World!\n");
     return 0;
 }
