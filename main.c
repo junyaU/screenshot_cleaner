ab120548ec67089a23d788cb90007033ddef6ef9
junya uchizono <junya19991215@icloud.com>
2022-10-27 22:24:53 +0900

    fix

--- main.c
+++ main.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <dirent.h>
 
 #define PATH_SIZE 512
 
 int main() {
-    char path_name[PATH_SIZE];
-    getcwd(path_name, PATH_SIZE);
-    printf("current dir : %s\n", path_name);
-    printf("Hello, World!\n");
+//    char path_name[PATH_SIZE];
+//    getcwd(path_name, PATH_SIZE);
+//    printf("current dir : %s\n", path_name);
+//    printf("Hello, World!\n");
+//
+//    char filename[] = "/Users/uchienuruunari/Dockerfile";
+//    int result = remove(filename);
+//
+//    printf("%s", result);
+
+    DIR *dir;
+    struct dirent *dp;
+    char dir_path[] = "/Users/uchienuruunari";
+    dir = opendir(dir_path);
+
+    if (dir == NULL) {
+        return 1;
+    }
+
+    dp = readdir(dir);
+    while (dp != NULL) {
+        printf("%s\n", dp->d_name);
+        dp = readdir(dir);
+    }
+
+    if (dir != NULL) {
+        closedir(dir);
+    }
+
     return 0;
 }
